dff05f2093c107f53758029b2b318e75dd55f2e0
bootypix <123077722+bootypix@users.noreply.github.com>
2023-01-19 19:51:08 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
 import BootyPixPage from "../src/containers/bootypix";
-import HomeContainer from "../src/containers/home";
 
 const Home: NextPage = () => {
   return (
