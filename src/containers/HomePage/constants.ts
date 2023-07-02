c7304ba821500f6ae6b808f1d90b6194c153aa85
onlylabs <123077722+only-labs@users.noreply.github.com>
2023-07-02 22:47:40 +0200

    Update constants.ts

--- src/containers/HomePage/constants.ts
+++ src/containers/HomePage/constants.ts
@@ -1,7 +1,7 @@
 export const TITLE = "MINT DETAILS";
 
 export const HOME_CONTENT = [
-    "10,000 memeable models full of suprises.",
+    "10,000 memeable crypto models full of suprises.",
     "0.003 per model. First 1000 are free."
 ];
 
